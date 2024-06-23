d59b47c8d17e6812f46de548509707122c7a790f
Ramlatch <info@robinmaurer.com>
2024-06-23 22:20:55 +0200

    batchfile

---
+++ train_horeka.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash 
+
+#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=1
+
+#SBATCH -J modulus_Test
+#SBATCH --output="/hkfs/work/workspace/scratch/vm6493-fourcastnet/fourcastnet/results/slurm_logs/test_modulus/slurm-%j"
+#SBATCH -p accelerated
+#SBATCH --mem=501600mb
+#SBATCH --exclusive
+
+ml purge
+ml load compiler/intel/2023.1.0
+ml load mpi/openmpi/4.1
+
+BASE_DIR="/hkfs/work/workspace/scratch/"
+config_file="${BASE_DIR}/modulus/examples/weather/fcn_afno/conf/config.yaml"
+TRAIN_FILE="${BASE_DIR}/modulus/examples/weather/fcn_afno/train_era5_MA.py"
+
+
+export HDF5_USE_FILE_LOCKING=FALSE
+export NCCL_NET_GDR_LEVEL=PHB
+export MASTER_ADDR=$(scontrol show hostnames $SLURM_NODELIST | head -n 1)
+
+SRUN_PARAMS=(
+  --mpi="pmi2"
+  --gpus-per-task=1
+  --gpu-bind="closest"
+  --label
+)
+
+source $BASE_DIR/.venvs/Modulus/bin/activate
+srun -u --mpi=pmi2 bash -c " 
+  python $TRAIN_FILE"
