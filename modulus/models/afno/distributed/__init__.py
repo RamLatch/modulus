450a6a1fc4178f1913eef09adac0fee0a461b7be
Ramlatch <info@robinmaurer.com>
2024-06-24 18:01:17 +0200

    forgot the relative import in __init__

--- modulus/models/afno/distributed/__init__.py
+++ modulus/models/afno/distributed/__init__.py
@@ -14,4 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#from .afno import DistributedAFNO
+from .afno import DistributedAFNO
+from .afno_mpi import DistributedAFNOMPI
