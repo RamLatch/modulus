722dbd4fc0ef572754ecb057c0288cb345156bf0
Ramlatch <info@robinmaurer.com>
2024-06-24 17:13:45 +0200

    dev_accelerated

--- train_horeka.sh
+++ train_horeka.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-#SBATCH --time=06:00:00
+#SBATCH --time=01:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
@@ -8,16 +8,16 @@
 
 #SBATCH -J modulus_Test
 #SBATCH --output="/hkfs/work/workspace/scratch/ie5012-MA/results/slurm_logs/test_modulus/slurm-%j"
-#SBATCH -p accelerated
+#SBATCH -p dev_accelerated
 #SBATCH --mem=501600mb
 #SBATCH --exclusive
+#SBATCH --mail-type=ALL
 
 ml purge
 ml load compiler/intel/2023.1.0
 ml load mpi/openmpi/4.1
 
 BASE_DIR="/hkfs/work/workspace/scratch/ie5012-MA"
-config_file="${BASE_DIR}/modulus/examples/weather/fcn_afno/conf/config.yaml"
 TRAIN_FILE="${BASE_DIR}/modulus/examples/weather/fcn_afno/train_era5_MA.py"
 
 
@@ -32,7 +32,7 @@ export MASTER_ADDR=$(scontrol show hostnames $SLURM_NODELIST | head -n 1)
 #   --label
 # )
 
-# source $BASE_DIR/.venvs/Modulus/bin/activate
+source $BASE_DIR/.venvs/Modulus/bin/activate
 # srun -u --mpi=pmi2 bash -c " 
 #   python $TRAIN_FILE"
-python $TRAIN_FILE
+srun python $TRAIN_FILE
