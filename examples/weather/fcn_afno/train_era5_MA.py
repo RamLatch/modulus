b136a17bdb779df7e04409cf80c02e39fb93d6f5
Ramlatch <info@robinmaurer.com>
2024-07-08 16:06:36 +0200

    dummy false

--- examples/weather/fcn_afno/train_era5_MA.py
+++ examples/weather/fcn_afno/train_era5_MA.py
@@ -96,7 +96,7 @@ def validation_step(eval_step, fcn_model, datapipe, channels=[0, 1], epoch=0):
     fcn_model.train()
     return loss_epoch / num_examples
 
-DUMMY = True
+DUMMY = False
 @hydra.main(version_base="1.2", config_path="conf", config_name="config" if not DUMMY else "dummy_config")
 def main(cfg: DictConfig) -> None:
 
