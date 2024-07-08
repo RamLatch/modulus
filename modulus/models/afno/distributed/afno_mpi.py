29884a7a27c08513e5bef5128a1ff58181dc0910
Ramlatch <info@robinmaurer.com>
2024-07-08 14:15:17 +0200

    dont generate pictures anymore

--- modulus/models/afno/distributed/afno_mpi.py
+++ modulus/models/afno/distributed/afno_mpi.py
@@ -27,7 +27,7 @@ if REPLICATE:
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     torch.set_printoptions(threshold=10_000)
-PRINTGRAPH = True
+PRINTGRAPH = False
 if PRINTGRAPH:
     from torchviz import make_dot
     GRAPHEPOCH = 0
