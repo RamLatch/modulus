f8835cc14ec9c6b4bdf705c82743d27abb978845
Ramlatch <info@robinmaurer.com>
2024-07-09 11:45:43 +0200

    mem format

--- modulus/distributed/utils_mpi.py
+++ modulus/distributed/utils_mpi.py
@@ -351,6 +351,8 @@ def all_gather_v_wrapper(
         return tensor
     
     t_dtype = tensor.dtype
+    t_mem = get_memory_format(tensor)
+    t_size = list(tensor.shape)
 
     # Determine local tensor size
     local_size = tensor.size(dim)
@@ -374,8 +376,8 @@ def all_gather_v_wrapper(
 
     # Reconstruct the global tensor
     # Assuming the tensor is 1D for simplicity. Adjust for actual dimensions.
-    global_tensor = torch.from_numpy(recv_buf).view(-1, *tensor.size()[1:]).to(tensor.device)
-    global_tensor=global_tensor.type(t_dtype)
+    global_tensor = torch.from_numpy(recv_buf).view(-1, t_size[1:]).to(tensor.device)
+    global_tensor=global_tensor.type(t_dtype).contiguous(memory_format=t_mem)
     return global_tensor
     # tensor_shape = list(tensor.shape)
     # tensor_format = get_memory_format(tensor)
