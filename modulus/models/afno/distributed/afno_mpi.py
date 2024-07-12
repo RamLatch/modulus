ccdd739c3e16fc40e86bf8236109bfb4f1519b78
Ramlatch <info@robinmaurer.com>
2024-07-12 22:16:42 +0200

    activated prints only for multi nodes

--- modulus/models/afno/distributed/afno_mpi.py
+++ modulus/models/afno/distributed/afno_mpi.py
@@ -173,23 +173,23 @@ class DistributedMLP(nn.Module):
         # gather if input is MP
         if self.comm.Get_size() > 1:
             if self.input_is_matmul_parallel:
-                print("rank", self.comm.Get_rank(), " in mlp before possible gather", x.shape)
+                if self.comm.Get_size() > 4:print("rank", self.comm.Get_rank(), " in mlp before possible gather", x.shape)
                 self.comm.Barrier()
                 x = gather_from_parallel_region(
                     x, dim=1, shapes=self.gather_shapes
                 )
-            print("rank", self.comm.Get_rank(), " in mlp after possible gather", x.shape)
+            if self.comm.Get_size() > 4:print("rank", self.comm.Get_rank(), " in mlp after possible gather", x.shape)
 
             x = copy_to_parallel_region(x)
-            print("rank", self.comm.Get_rank(), " in mlp after copy", x.shape)
+            if self.comm.Get_size() > 4:print("rank", self.comm.Get_rank(), " in mlp after copy", x.shape)
         x = F.conv2d(x, self.w1, bias=self.b1)
         x = self.act(x)
         x = self.drop(x)
         x = F.conv2d(x, self.w2, bias=None)
         if self.comm.Get_size() > 1:
-            print("rank", self.comm.Get_rank(), " in mlp before possible reduce", x.shape)
+            if self.comm.Get_size() > 4:print("rank", self.comm.Get_rank(), " in mlp before possible reduce", x.shape)
             x = reduce_from_parallel_region(x)
-            print("rank", self.comm.Get_rank(), " in mlp after possible reduce", x.shape)
+            if self.comm.Get_size() > 4:print("rank", self.comm.Get_rank(), " in mlp after possible reduce", x.shape)
         x = x + torch.reshape(self.b2, (1, -1, 1, 1))
         x = self.drop(x)
 
@@ -197,7 +197,7 @@ class DistributedMLP(nn.Module):
         if self.comm.Get_size() > 1:
             if self.output_is_matmul_parallel:
                 x = scatter_to_parallel_region(x, dim=1)
-                print("rank", self.comm.Get_rank(), " in mlp after possible scatter", x.shape)
+                if self.comm.Get_size() > 4:print("rank", self.comm.Get_rank(), " in mlp after possible scatter", x.shape)
         return x
 
 
@@ -533,21 +533,21 @@ class DistributedBlock(nn.Module):
             if not self.input_is_matmul_parallel:
                 
                 x = scatter_to_parallel_region(x, dim=1)
-        print("rank", self.rank, " in block after possible scatter", x.shape)
+        if self.world_size > 4:print("rank", self.rank, " in block after possible scatter", x.shape)
         residual = x
         x = self.norm1(x)
         x = self.filter(x)
-        print("rank", self.rank, " in block after filter", x.shape)
+        if self.world_size > 4:print("rank", self.rank, " in block after filter", x.shape)
 
         if self.double_skip:
             x = x + residual
             residual = x
 
         x = self.norm2(x)
-        print("rank", self.rank, " in block after norm2", x.shape)
+        if self.world_size > 4:print("rank", self.rank, " in block after norm2", x.shape)
 
         x = self.mlp(x)
-        print("rank", self.rank, " in block after mlp", x.shape)
+        if self.world_size > 4:print("rank", self.rank, " in block after mlp", x.shape)
 
         x = self.drop_path(x)
         x = x + residual
@@ -695,7 +695,7 @@ class DistributedAFNONet(nn.Module):
 
         # reshape
         x = x.reshape(B, self.embed_dim_local, self.h, self.w)
-        print("rank", self.rank, "before blocks", x.shape)
+        if self.comm.Get_size() > 4:print("rank", self.rank, "before blocks", x.shape)
         for blk in self.blocks:
             x = blk(x)
         return x
